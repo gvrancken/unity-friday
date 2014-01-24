cb09bb774d4b113140562b5e8c4abbb87b8398e1
ziccidus <gilbert@ziccidus.nl>
2014-01-24 11:59:25 +0100

    PlayerController aangemaakt

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("haai merijn!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
