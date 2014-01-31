43e7d6cfac72de2b8eda2fb22b220e7fbe6dd53e
Merijn Vogelsang <merijn.vogelsang@brightalley.nl>
2014-01-31 15:20:30 +0100

    Change shieldcolor on hit

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -2,18 +2,27 @@
 using System.Collections;
 
 public class Shield : MonoBehaviour {
-	public int damagePoints;
+	public int damagePoints = 10;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	void OnCollisionEnter (Collision col) {	
-		Debug.Log ("hit!");
-		if (col.gameObject.tag != "Enemy") {
+
+
 			DamageController dc = col.gameObject.GetComponent<DamageController>();
 			dc.takeDamage(damagePoints);
+		foreach (Transform child in transform) {
+			//Debug.Log ("hit!");
+			child.gameObject.renderer.material.color -= new Color (0.01f, 0.2f, 0.2f, 0.2f);
 		}
+	}
+
+	void Update () {
+		//DamageController dc = GetComponent<DamageController>();
+		//float HP = dc.getHP();
+
 	}
 
 
