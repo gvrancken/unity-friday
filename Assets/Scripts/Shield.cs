46616ec83fd42f864448ab20a37d6d8bbb88a85a
Merijn Vogelsang <merijn.vogelsang@brightalley.nl>
2014-01-31 16:42:13 +0100

    Added particles on shield collision

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Shield : MonoBehaviour {
 	public int damagePoints = 10;
+	public ParticleSystem explosion;
 	// Use this for initialization
 	void Start () {
-	
+		explosion.Play();
+
 	}
 	
 	void OnCollisionEnter (Collision col) {	
@@ -16,6 +18,10 @@ public class Shield : MonoBehaviour {
 		foreach (Transform child in transform) {
 			//Debug.Log ("hit!");
 			child.gameObject.renderer.material.color -= new Color (0.01f, 0.2f, 0.2f, 0.2f);
+
+			ParticleSystem explosionInst = (ParticleSystem)Instantiate(explosion, transform.position, transform.rotation);
+			explosionInst.Play();
+			Destroy(explosionInst.gameObject,1);
 		}
 	}
 
--- Assets/Scripts/ShieldManager.cs
+++ Assets/Scripts/ShieldManager.cs
@@ -4,27 +4,33 @@ using System.Collections;
 public class ShieldManager : MonoBehaviour {
 
 	public Transform shieldPiece;
-	public int max = 30;
-	public float tetha = 20;
-	public float alpha = 5;
+	public int max = 45;
+	public float tetha = 30;
+	public float alpha = 4;
+
 
 
 	// Use this for initialization
 	void Start () {
 		//float tetha = 6+ (max * 6);
 		Debug.Log (6+ (max * 6));
-		for (int i = 5; i <= max+5; i++) {
-
-			float t = (tetha/max)*i;
-			float a = (alpha/max)*i;
-			Vector3 newPosition = new Vector3(transform.position.x + a*Mathf.Cos(t), 0, transform.position.y + a*Mathf.Sin(t));
-			Transform instance = (Transform)Instantiate(shieldPiece, newPosition, transform.rotation);
-			instance.LookAt(transform);
-			float scaleFactor = (float)0.1*i;
-			instance.localScale += new Vector3(scaleFactor, 0, 0	);
+		for (int i = 1; i <= max; i++) {
+			createShield(i);
 		}
 	}
-	
+
+	void createShield(int i) {
+		i += 5;
+		float t = (tetha/max)*i;
+		float a = (alpha/max)*i;
+		Vector3 newPosition = new Vector3(transform.position.x + a*Mathf.Cos(t), 0, transform.position.y + a*Mathf.Sin(t));
+		Transform instance = (Transform)Instantiate(shieldPiece, newPosition, transform.rotation);
+		instance.LookAt(transform);
+		float scaleFactor = (float)0.1*i;
+		instance.localScale += new Vector3(scaleFactor, 0, 0	);
+		instance.parent = transform;
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
