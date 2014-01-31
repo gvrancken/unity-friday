4ee3f1c0aa3a50a7b06742397d9715c814a49ad3
ziccidus <gilbert@ziccidus.nl>
2014-01-31 10:46:48 +0100

    SpawnerScript test

---
+++ Assets/SpawnerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour {
+
+	public Transform prefab;
+	public int spawnTime; 
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Clone", spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void Clone () {
+		Instantiate(prefab, Vector3.zero, Quaternion.identity);
+	}
+}
