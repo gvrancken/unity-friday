b5cfe4fa6dfc8e22d71726b90b973089e08df9e7
ziccidus <gilbert@ziccidus.nl>
2014-01-31 15:25:07 +0100

    damage na 0.5 sec

--- Assets/Scripts/DamageController.cs
+++ Assets/Scripts/DamageController.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class DamageController : MonoBehaviour {
 
-	public float MaxHitPoints = 100;
+	public float MaxHitPoints = 1;
 	private float _hitpoints;
+	private bool _isDead = false;
 
 	void Start () {
 		_hitpoints = MaxHitPoints;
@@ -13,12 +14,21 @@ public class DamageController : MonoBehaviour {
 	public void takeDamage(float damagePoints) {
 		_hitpoints -= damagePoints;
 
-		Debug.Log ("I have " + _hitpoints + " left!");
+		Debug.Log (gameObject.name + " has " + _hitpoints + " left!");
 
-		if (_hitpoints <= 0) {
-			Destroy (gameObject);
+		if (_isDead == false && _hitpoints <= 0) {
+			_isDead = true;
+			Invoke ("DestroyMe", 1f);
+
+			Debug.Log ("will die!");
+			Invoke ("DestroyMe", 1);
 		}
 
-
 	}
+
+	void DestroyMe() {
+		Debug.Log ("bye!");
+		gameObject.SetActive(false);
+	}
+
 }
