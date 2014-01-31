c20c026e342aff55a28d7668cdbf1355187d30d8
Merijn Vogelsang <merijn.vogelsang@brightalley.nl>
2014-01-31 13:10:38 +0100

    Added ShieldManager & Shield prefab

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter (Collision col) {
+		col.gameObject.tag == "Enemy")
+		{
+			transform.localScale = Vector3(0.5, 0.5, 0.5);
+			col.gameObject;
+		}
+	}
