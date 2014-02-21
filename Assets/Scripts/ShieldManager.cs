5df9e010224ab8b5ade80e290f025107d0b1ce26
unknown <mvogelsang@LT1045.group.wan>
2014-02-21 16:39:52 +0100

    Changed shield routine

--- Assets/Scripts/ShieldManager.cs
+++ Assets/Scripts/ShieldManager.cs
@@ -4,11 +4,28 @@ using System.Collections;
 public class ShieldManager : MonoBehaviour {
 
 	public Transform shieldPiece;
+	public Transform core;
+	[Range(1,500)]
 	public int startCount = 25; 
-	public int max = 45;
-	public float tetha = 30;
+	[Range(1,1000)]
+	public int max = 1000;
+	[Range(1f,2000f)]
+	public float tetha = 900;
+	[Range(-500f,1000f)]
 	public float alpha = 4;
+	[Range(0.1f,100f)]
+	public float alphaScale = 2;
+	[Range(0f,90f)]
+	public float shieldRotate = 2.5f;
+	[Range(0f,25f)]
+	public float shieldThickness = 2f;
+	[Range(0f,2f)]
+	public float shieldLengthScale = 0.2f;
+	[Range(0f,15f)]
+	public float shieldLengthFactor = 0.2f;
 
+
+	private Vector3[] shieldJoints; 
 	private GameObject[] shieldArray; 
 	private int totalShields = 0;
 	private int firstBrokenPos = 0;
@@ -20,9 +37,23 @@ public class ShieldManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//float tetha = 6+ (max * 6);
-		shieldArray = new GameObject[max];
-		for (int i = 0; i <= startCount-1; i++) {
+			shieldJoints = new Vector3[max + 1];
+			shieldArray = new GameObject[max];
+			initializeSchieldJoints ();
+			rearrangeShields();
+	}
+
+	void OnDrawGizmosSelected() {
+		for (int i = 0; i<=max; i++) {
+				Gizmos.color = Color.white;
+				Gizmos.DrawWireSphere (shieldJoints[i], 1);
+
+		}
+	}
+
+	void rearrangeShields() {
+
+		for (int i = 0; i <= startCount; i++) {
 			GameObject newShield = createShield(i);
 			shieldArray[i] = newShield;
 			newShield.GetComponent<Shield>().setEnergized(false);
@@ -30,22 +61,32 @@ public class ShieldManager : MonoBehaviour {
 		}
 	}
 
+	void initializeSchieldJoints() {
+		for (int i = 0; i<shieldJoints.Length; i++) {
+			float localAlpha = alpha + (alphaScale *(i+6));
+			float t = (tetha / max) * (i+6);
+			float a = (localAlpha / max) * (i+6);
+			Vector3 newPosition = new Vector3 (transform.position.x + a * Mathf.Cos (t), 0, transform.position.y + a * Mathf.Sin (t));
+			shieldJoints[i] = newPosition;
+		}
+	}
+
 	public GameObject createShield(int i) {
-		i += 6;
-		float t = (tetha/max)*i;
-		float a = (alpha/max)*i;
-		Vector3 newPosition = new Vector3(transform.position.x + a*Mathf.Cos(t), 0, transform.position.y + a*Mathf.Sin(t));
-		Transform instance = (Transform)Instantiate(shieldPiece, newPosition, transform.rotation);
+		//Take position inbetween 2 shieldJoints
+		Vector3 shieldPosition = (shieldJoints [i] + shieldJoints [i + 1])/2;
+		Transform instance = (Transform)Instantiate(shieldPiece, shieldPosition, transform.rotation);
 		instance.LookAt(transform);
-		instance.transform.Rotate (new Vector3 (0, 2.5f, 0));
-		float scaleFactor = (float)0.2*i;
-		instance.localScale += new Vector3(scaleFactor, 0, 2f);
+		instance.transform.Rotate(0, shieldRotate, 0);
+		float scaleFactor = Vector3.Distance (shieldJoints [i], shieldJoints [i+1]);
+		instance.localScale = new Vector3(scaleFactor/2, instance.localScale.y, (float)instance.localScale.z*(1f+(i/10f)) );
 		instance.parent = transform;
 		return instance.gameObject;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		initializeSchieldJoints ();
+		//rearrangeShields ();
 		//Check if the shield is continues and de-energize all seperate parts
 		for (int i = 0; i <=shieldArray.Length-1; i++) {
 			if ((shieldArray[i]==null) && (firstBrokenPos > i)) {
@@ -54,24 +95,23 @@ public class ShieldManager : MonoBehaviour {
 				shieldArray[i].GetComponent<Shield>().setEnergized(false);
 			}
 		}
-
+//			
 		//Pulse the central crystal
 		pulseState = Mathf.Sin (Time.time*5);
 		pulseState = 1f + pulseState / 5;
-		gameObject.renderer.material.SetFloat("_RimPower", pulseState);
+		core.renderer.material.SetFloat("_RimPower", pulseState);
 
 		if (damageEffect > 0) {
 			damageEffect -= 0.1f * Time.deltaTime;
-			Debug.Log("damage effect = " + damageEffect);
 			Color currentColor = ((1-damageEffect)*defaultColor)+(damageEffect*damageColor);
-			gameObject.renderer.material.SetFloat ("_RimPower", ((1-damageEffect)*pulseState)+(0));
-			gameObject.renderer.material.SetColor("_RimColor", currentColor);
+			core.renderer.material.SetFloat ("_RimPower", ((1-damageEffect)*pulseState)+(0));
+			core.renderer.material.SetColor("_RimColor", currentColor);
 
 			
 		}
 
-
 		//Pulse shield pieces with delay
+		Debug.Log(firstBrokenPos);
 		for (int i = 0; i <firstBrokenPos; i++) {
 			if (shieldArray[i]!=null){
 				pulseState = Mathf.Sin(((i*10) + Time.time) * 5);
