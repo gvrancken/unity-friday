398e44961e3af43ceb4629ea5026d22785129ae7
ziccidus <gilbert@ziccidus.nl>
2014-03-07 12:12:17 +0100

    added clickController clickController in levelManager levelManager now a prefab

---
+++ Assets/Scripts/ClickController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonUp(0)) {
+			// create a ray going into the scene from the screen location the user clicked at
+			Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+			
+			// the raycast hit info will be filled by the Physics.Raycast() call further
+			RaycastHit hit;
+			
+			// perform a raycast using our new ray. 
+			// If the ray collides with something solid in the scene, the "hit" structure will
+			// be filled with collision information
+			if( Physics.Raycast( ray, out hit ) )
+			{
+				// a collision occured. Check it.
+				Debug.Log ("clicked: " + hit.transform.gameObject);
+				if (hit.transform.tag == "Tower") {
+
+
+				}
+
+			}
+
+		}
+
+			
+	}
+}
