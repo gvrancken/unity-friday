6ff646468d72ef3be07eb265bbcb06f85471d437
ziccidus <gilbert@ziccidus.nl>
2014-03-25 14:32:01 +0100

    Added Turret Line of sight

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -45,15 +45,18 @@ public class TurretController : MonoBehaviour {
 		}
 
 		if (target != null) {
+			if (IsTargetInSight(target)) {
+				Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+				transform.rotation = rotation;
 
-			Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position);
-			transform.rotation = rotation;
+				Vector3 targetDir = target.transform.position - transform.position;
+				float angle = Vector3.Angle (transform.forward, targetDir);
 
-			Vector3 targetDir = target.transform.position - transform.position;
-			float angle = Vector3.Angle (transform.forward, targetDir);
-
-			if (angle < 5f && canShoot) {
-				ShootBullet();
+				if (angle < 5f && canShoot) {
+					ShootBullet();
+			} else {
+					target = null;
+				}
 			}
 		} else {
 			targetsInRange.Remove(null);
@@ -63,6 +66,27 @@ public class TurretController : MonoBehaviour {
 			}
 		}
 
+	}
+
+	bool IsTargetInSight(GameObject tempTarget) {
+		Ray ray = new Ray(transform.position, (tempTarget.transform.position - transform.position));
+		
+		// the raycast hit info will be filled by the Physics.Raycast() call further
+		RaycastHit hit;
+		
+		// perform a raycast using our new ray. 
+		// If the ray collides with something solid in the scene, the "hit" structure will
+		// be filled with collision information
+		if( Physics.Raycast( ray, out hit ) )
+		{
+			// a collision occured. Check it.
+			if (hit.transform.gameObject == tempTarget) {
+				target = tempTarget;
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 	void ShootBullet() {
