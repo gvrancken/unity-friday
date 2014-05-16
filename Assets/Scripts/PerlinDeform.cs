f3f3b59a2f3f478eaf79ff6316e6c89da1b3aad4
ziccidus <gilbert@ziccidus.nl>
2014-05-16 16:17:57 +0200

    bubbles and germs

--- Assets/Scripts/PerlinDeform.cs
+++ Assets/Scripts/PerlinDeform.cs
@@ -8,8 +8,8 @@ using TreeEditor;
 public class PerlinDeform : MonoBehaviour {
 
 
-	public float scale = 0.3f;
-	public float speed = 0.3f;
+	public float scale = 0.2f;
+	public float speed = 0.6f;
 	public bool recalculateNormals = false;
 
 	private Vector3[] baseVertices;
@@ -34,19 +34,25 @@ public class PerlinDeform : MonoBehaviour {
 		float time_x = Time.time * speed + 0.1365143f;
 		float time_y = Time.time * speed + 1.21688f;
 		float time_z = Time.time * speed + 2.5564f;
-		
-		for (int i=0; i<vertices.Length; i++) {
+
+		Color[] colors = new Color[vertices.Length];
+
+		for (int i=0; i < vertices.Length; i++) {
 			
 			Vector3 vertex = baseVertices[i];
 
 			vertex.x += noise.Noise(time_x + vertex.x, time_x + vertex.y, time_x + vertex.z) * scale;
 			vertex.y += noise.Noise(time_y + vertex.x, time_y + vertex.y, time_y + vertex.z) * scale;
 			vertex.z += noise.Noise(time_z + vertex.x, time_z + vertex.y, time_z + vertex.z) * scale;
-			
+			colors[i] = Color.Lerp(Color.red, Color.green, vertices[i].y);
+
+
 			vertices[i] = vertex;
 
 		}
-		
+
+		mesh.colors = colors;
+
 		mesh.vertices = vertices;
 		
 		if (recalculateNormals) {
